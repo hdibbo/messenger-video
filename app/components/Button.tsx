02cffac83c2303cd93f85ae36114f36575955431
hdibbo <50245912+hdibbo@users.noreply.github.com>
2024-03-27 02:54:57 -0400

    Update Button.tsx

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -42,7 +42,7 @@ const Button: React.FC<ButtonProps> = ({
         `,
         
         disabled && "opacity-50 cursor-default",
-        fullwidth && "w-full",
+        fullWidth && "w-full",
         secondary ? 'text-gray-900' : 'text-white',
         danger && "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
         !secondary && !danger && "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600"
